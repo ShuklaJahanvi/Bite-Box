a8540b38cfdfaa55179954baf30bb51c59c041d2
ShuklaJahanvi <166206912+ShuklaJahanvi@users.noreply.github.com>
2024-09-25 11:55:21 -0400

    Add files via upload

---
+++ src/pages/Resturant/Resturant.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Resturant.css'
+
+const Resturant = () => {
+  return (
+    <div>
+      <h1>welcome to the resturant.</h1>
+    </div>
+  )
+}
+
+export default Resturant
